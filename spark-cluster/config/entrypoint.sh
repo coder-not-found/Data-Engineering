62334ddfe94cce77b29b16af9599bfc87d21b8f2
ninja <48689580+coder-not-found@users.noreply.github.com>
2025-12-15 21:21:34 +0100

    Update entrypoint.sh

--- spark-cluster/config/entrypoint.sh
+++ spark-cluster/config/entrypoint.sh
@@ -1,44 +1,77 @@
 #!/bin/bash
-
+ 
 # Change dns config
-echo "Updating DNS Config"
-NETWORK="tailscale0"
 
+ echo "Updating DNS Config"
+
+ NETWORK="tailscale0"
+ 
 if [ -d "/sys/class/net/$NETWORK" ]; then
-	sed -i "/dfs.datanode.dns.interface/{n;s/default/$NETWORK/}" $HADOOP_CONF_DIR/hdfs-site.xml
-fi
 
+         sed -i "/dfs.datanode.dns.interface/{n;s/default/$NETWORK/}" $HADOOP_CONF_DIR/hdfs-site.xml
+
+ fi
+ 
 # Start services in the background
-service ssh start
 
+ service ssh start
+ 
 HOSTNAME=$(hostname)
-# Format on the first start
-if [ "$HOSTNAME" = "master-node" ] && [ ! -d "/home/hadoop/data/nameNode/current" ]; then
-	$HADOOP_HOME/bin/hadoop namenode -format
-fi
-		
-echo "Starting HDFS and Yarn"
-$HADOOP_HOME/sbin/start-dfs.sh
-sleep 5
-$HADOOP_HOME/sbin/start-yarn.sh
-sleep 5
 
+ # Format on the first start
+
+ if [ "$HOSTNAME" = "master-node" ] && [ ! -d "/home/hadoop/data/nameNode/current" ]; then
+
+         $HADOOP_HOME/bin/hadoop namenode -format
+
+ fi
+ 
 if [ "$HOSTNAME" = "master-node" ]; then
-	$SPARK_HOME/sbin/start-master.sh
 
-	# Starts history server to check running and completed applications
-	$HADOOP_HOME/bin/hdfs dfs -mkdir -p /spark-logs
-	$SPARK_HOME/sbin/start-history-server.sh
+         echo "Starting HDFS and Yarn on Master"
 
-	# Disables safe mode to prevent errors in small clusters
-	$HADOOP_HOME/bin/hdfs dfsadmin -safemode leave
-	
-	echo "Start Jupyter..."
-	nohup jupyter notebook --allow-root --no-browser --NotebookApp.token='' --ip='*' > /home/big_data/jupyter.log &
-else
-	# Sleeps to prevent connection issues with master
-    sleep 5
-    $SPARK_HOME/sbin/start-worker.sh master-node:7077
-fi
-    	
-sleep infinity
+         $HADOOP_HOME/sbin/start-dfs.sh
+
+         sleep 5
+
+         $HADOOP_HOME/sbin/start-yarn.sh
+
+         sleep 5
+
+         $SPARK_HOME/sbin/start-master.sh -h 100.123.239.104
+ 
+        # Starts history server to check running and completed applications
+
+         $HADOOP_HOME/bin/hdfs dfs -mkdir -p /spark-logs
+
+         $SPARK_HOME/sbin/start-history-server.sh
+ 
+        # Disables safe mode to prevent errors in small clusters
+
+         $HADOOP_HOME/bin/hdfs dfsadmin -safemode leave
+
+         echo "Start Jupyter..."
+
+         nohup jupyter notebook --allow-root --no-browser --NotebookApp.token='' --ip='*' > /home/big_data/jupyter.log &
+
+ else
+
+         echo "Starting DataNode and NodeManager on Worker"
+
+         # Start only datanode (not full DFS)
+
+         $HADOOP_HOME/bin/hdfs --daemon start datanode
+
+         sleep 5
+
+         # Start only nodemanager (not full YARN)
+
+         $HADOOP_HOME/bin/yarn --daemon start nodemanager
+
+         sleep 5
+
+         $SPARK_HOME/sbin/start-worker.sh spark://100.123.239.104:7077
+
+ fi
+
+ sleep infinity
